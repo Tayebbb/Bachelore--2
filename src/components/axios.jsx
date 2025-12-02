0328d854a8019a710f5fd40f3da35db519d47bde
tanjim@041 <tanjim433@gmail.com>
2025-12-02 12:11:35 +0600

    first commit

--- src/components/axios.jsx
+++ src/components/axios.jsx
@@ -1,26 +1,48 @@
-import axios from 'axios';
+import axios from "axios";
 
+const BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: BASE,
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json",
   },
   timeout: 5000,
 });
-// Debugging: log requests and responses to help trace login failures
-instance.interceptors.request.use(req => {
-  try{
-    console.debug('[axios] Request:', req.method, req.url, req.data ? req.data : '')
-  }catch(e){}
-  return req
-}, err => { console.debug('[axios] Request error', err); return Promise.reject(err) })
+instance.interceptors.request.use(
+  (req) => {
+    try {
+      console.debug(
+        "[axios] Request:",
+        req.method,
+        req.url,
+        req.data ? req.data : ""
+      );
+    } catch (e) {}
+    return req;
+  },
+  (err) => {
+    console.debug("[axios] Request error", err);
+    return Promise.reject(err);
+  }
+);
 
-instance.interceptors.response.use(res => {
-  try{ console.debug('[axios] Response:', res.status, res.config.url, res.data) }catch(e){}
-  return res
-}, err => {
-  try{ console.debug('[axios] Response error:', err.response?.status, err.response?.data) }catch(e){}
-  return Promise.reject(err)
-})
+instance.interceptors.response.use(
+  (res) => {
+    try {
+      console.debug("[axios] Response:", res.status, res.config.url, res.data);
+    } catch (e) {}
+    return res;
+  },
+  (err) => {
+    try {
+      console.debug(
+        "[axios] Response error:",
+        err.response?.status,
+        err.response?.data
+      );
+    } catch (e) {}
+    return Promise.reject(err);
+  }
+);
 
 export default instance;
